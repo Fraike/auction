24937e448198e79557e63d0b9a23a3fb39e8ff86
wwt <648128145@qq.com>
2019-03-15 18:00:31 +0800

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,16 +9,18 @@ export class ProductComponent implements OnInit {
 
   private products: Array<Product>;
 
+  private imgUrl = "../../assets/images/image.jpg"
+
   constructor() { }
 
   ngOnInit() {
     this.products = [
       new Product(1,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
-      new Product(2,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
-      new Product(3,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
-      new Product(4,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
-      new Product(5,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
-      new Product(6,"第一个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
+      new Product(2,"第二个商品",1.99,3.5,"阿萨发的",["电子产品","数码"]),
+      new Product(3,"第三个商品",1.99,4.5,"阿萨发的",["电子产品","数码"]),
+      new Product(4,"第四个商品",1.99,1.5,"阿萨发的",["电子产品","数码"]),
+      new Product(5,"第五个商品",5,3.5,"阿萨发的",["电子产品","数码"]),
+      new Product(6,"第六个商品",3.99,3.5,"阿萨发的",["电子产品","数码"]),
      
     ]
   }
